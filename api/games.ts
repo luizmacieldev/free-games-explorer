c2ed9034bd571e1b5680aee6648fac87ff6415cc
luizlgam <lgamjoeperry@gmail.com>
2026-04-16 17:34:48 -0300

    Added function to handle parameters

--- api/games.ts
+++ api/games.ts
@@ -1,15 +1,16 @@
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
   try {
-    const query = req.url.split("?")[1] || "";
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const id = url.searchParams.get("id");
 
     const response = await fetch(
-      `https://www.freetogame.com/api/games?${query}`
+      `https://www.freetogame.com/api/game?id=${id}`
     );
 
     const data = await response.json();
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch games" });
+    res.status(500).json({ error: "Failed to fetch game details" });
   }
 }
